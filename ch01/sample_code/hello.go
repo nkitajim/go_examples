ea29aee337a6642943d10986a72995abfbe53a4c
nkitajim <nkitajim@gmail.com>
2025-08-27 15:50:58 +0900

    add comment

--- ch01/sample_code/hello.go
+++ ch01/sample_code/hello.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Hello", "World!")
-	// fmt.Println("Hello %s!", "World")
+	// fmt.Println("Hello %s!", "World") // vet error, %sを使うべき関数ではない
 	fmt.Println("")
 	fmt.Printf("Hello %s!", "World")
 	fmt.Printf("Hello %s!\n", "World")
-	// fmt.printf("Hello %s!", "World")
+	// fmt.printf("Hello %s!", "World") // not found
 }
