81393eed21f8a6683788c5eef5354ef09a81f9fa
nkitajim <nkitajim@gmail.com>
2025-08-27 15:49:04 +0900

    add ch01

---
+++ ch01/sample_code/go.mod
@@ -0,0 +1,3 @@
+module github.com/go_examples/ch01/sample_code
+
+go 1.25.0
---
+++ ch01/sample_code/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("Hello", "World!")
+	// fmt.Println("Hello %s!", "World")
+	fmt.Println("")
+	fmt.Printf("Hello %s!", "World")
+	fmt.Printf("Hello %s!\n", "World")
+	// fmt.printf("Hello %s!", "World")
+}
