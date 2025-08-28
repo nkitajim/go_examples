406f8c15835e61a984f900ec907da06fcae0c160
nkitajim <nkitajim@gmail.com>
2025-08-28 15:22:13 +0900

    add ch04/ex3

---
+++ ch04/exercise_solutions/ex3/ex3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var total int
+
+	for i := 0; i < 10; i++ {
+		// total := total + i // bad case
+		total = total + i
+		fmt.Printf("i=%v, total=%v\n", i, total)
+	}
+	fmt.Printf("total=%v\n", total)
+}
