0e93cd077f8b992d20b9084dbcd4a1f86e597519
nkitajim <nkitajim@gmail.com>
2025-09-01 08:05:15 +0900

    fix main.go

---
+++ ch07/exercise_solutions/sp3/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Logger interface {
+	Log(s string)
+}
+
+type Log1 struct{}
+
+func (l Log1) Log(s string) {
+	fmt.Println("[Log1]", s)
+}
+
+type Log2 struct{}
+
+func (l Log2) Log(s string) {
+	fmt.Println("[Log2]", s)
+}
+
+func Log3(s string) {
+	fmt.Println("[Log3]", s)
+}
+
+func Log4(s string) {
+	fmt.Println("[Log4]", s)
+}
+
+type Log5 struct {
+	l Logger
+}
+
+func (l Log5) Print(s string) {
+	l.l.Log(s)
+}
+
+func main() {
+	{
+		log := Log1{}
+		log.Log("hello")
+	}
+
+	{
+		log := Log2{}
+		log.Log("hello")
+	}
+
+	{
+		Log3("hello")
+		Log4("hello")
+	}
+
+	{
+		e1 := Log5{Log1{}}
+		e1.Print("Hello")
+
+		e2 := Log5{Log2{}}
+		e2.Print("Hello")
+	}
+}
